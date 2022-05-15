4ac07d3b6c0c061b8f6d3854499cc6ede26abe82
rhipps <ryanhippsdesign@gmail.com>
2022-05-15 12:15:42 -0400

    Read choices from a remote location

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Wheel of Dinner 
+
+This is a wheel of dinner for Columbus Ohio, it spins and picks your dinner. The wheels decision is absolute.
+
+---
+
+Wheel JS Modified from https://github.com/shekharramola/react-wheel-of-prizes
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Wheel from './components/wheel';
-import { Segment } from './components/wheel-component';
+import Wheel, { Segment } from './components/wheel';
 
+const colorCodes = ['#E49091', '#80CF94', '#7888D2']
+const configUrl = "https://gist.githubusercontent.com/rhipps/553eda6ba674da0260bce215c24bb191/raw/05892249bb922cf44d80b6ab0b4c3b884e35c9f8/cbus_resteraunts"
 
+const createResterauntSegments = (resterauntNames: string[]): Segment[] => {
+  let tiles: Segment[] = []
+  resterauntNames.forEach((name, idx) => tiles.push({ segmentText: name, segmentColorCode: colorCodes[idx % colorCodes.length] }) )
+  return tiles
+}
 
-const segments: Segment[] = [
-  {segmentText: '123McDonalds', segmentColorCode: '#EE4040'},
-  {segmentText: 'Wendy\'s', segmentColorCode: '#F0CF50'},
-  {segmentText: 'Raising Canes', segmentColorCode: '#3DA5E0'},
- ]
-
-for (let i = 0; i <= 4; i++) segments.push({segmentText: `tester${i}`, segmentColorCode: '#555544'})
+const parseOutResterauntNames = (data: any) => data ? data.map((resterauntAndTags: Array<string>) => resterauntAndTags[0]) : []
 
 function App() {
+  const [names, setNames] = useState<Array<string>>([])
+  
+  useEffect(() => {
+    fetch(configUrl, {method: 'GET'})
+      .then(response => response.json())
+      .then(data => setNames(parseOutResterauntNames(data.data)))
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <Wheel 
-            onFinishedCallBack={() => {}}
-            segments={segments}    
-        ></Wheel>
       </header>
+      <div>
+        <Wheel
+            segments={createResterauntSegments(names)}
+            onFinished={() => {}}
+            durationFactor={300}
+            displayWinningText
+            canvasConfig={{width: 800, height:800, wheelPositionX: 400, wheelPositionY: 400}}
+            wheelConfig={{radius: 250, primaryColor: 'black', secondaryColor: 'white', fontFamily: 'Arial', buttonText: 'Spin', spinButtonRadius: 50}}
+        />
+      </div>
     </div>
   );
 }
